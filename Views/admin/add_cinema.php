fdf4f1338bfbc10500709da76529d5898266e655
Matias Ezequiel Miguel Rovedo <matute.miguel.98@gmail.com>
2020-10-11 19:09:00 -0300

    Views del admin para consultar cine, pelicula agregadas

---
+++ Views/admin/add_cinema.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Agregar Cine</h1>
+    <form action='' method='post'>
+        <h1>Cine:</h1>
+        <input type="text" name="cinemaName" placeholder="Nombre del Cine">
+        <h1>Capacidad</h1>
+        <input type="text" name="cinemaCapacity" placeholder="Capacidad del Cine">
+        <h1>Direccion</h1>
+        <input type="text" name="cinemaAdress" placeholder="Direccion del Cine">
+        <h1>Valor de la Entrada</h1>
+        <input type="text" name="cinemaValue" placeholder="Valor del Cine">
+
+        <button type="submit">Aceptar</button>
+    </form>
+</body>
+</html>
