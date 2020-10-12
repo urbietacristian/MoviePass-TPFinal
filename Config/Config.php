35bb81a0509ac74364a418d142d479a7ed084bc4
carlitox714 <lucio.chapaman@gmail.com>
2020-10-12 10:29:48 -0300

    startLoginFacebook

--- Config/Config.php
+++ Config/Config.php
@@ -1,5 +1,15 @@
 <?php namespace Config;
 
+require_once __DIR__ . '/vendor/autoload.php';
+
+$fb = new Facebook\Facebook([
+    'app_id' => '813085052788956',
+    'app_secret' => 'efd7113ea0a3119968ce477683bbb9ac',
+    'default_graph_version' => 'v2.10',
+    ]);
+
+
+$handler = $FBObject -> getRedirectLoginHelper();
 define("ROOT", dirname(__DIR__) . "/");
 //Path to your project's root folder
 define("FRONT_ROOT", "/UTN/TP6/ExerciseThree/");
