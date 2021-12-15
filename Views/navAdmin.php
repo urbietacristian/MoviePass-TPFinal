d1273ebb89ea485782191558e2e5ba21ec11b691
luciochiappa <luciochiappa.98@gmail.com>
2021-12-15 00:53:08 -0300

    Signed-off-by: luciochiappa <luciochiappa.98@gmail.com>

--- Views/navAdmin.php
+++ Views/navAdmin.php
@@ -9,21 +9,21 @@
     }
 ?>
 
-<div class="wrapper row1">
-  <header id="header" class="hoc clear"> 
+<div class="nav">
+  <header > 
     <div style="vertical-align: middle;" id="logo" class="imgl">
       <h1><a href="<?php echo FRONT_ROOT; ?>Movie/ShowMovies"><img  src="<?php echo IMG_PATH."logo.png"?>" > Movie Pass</a></h1>
     </div>    
-    <nav id="mainav" class="fl_right">
-      <li class="active"><a href="<?php echo FRONT_ROOT; ?>Purchase/ShowSalesView">Totales vendidos</a></li>
-      <li class="active"><a href="<?php echo FRONT_ROOT; ?>Purchase/ShowTicketsSoldView">Tickets vendidos</a></li>
-      <li class="active"><a href="<?php echo FRONT_ROOT; ?>Movie/showMovies">Listar Peliculas</a></li>
-      <li class="active"><a href="<?php echo FRONT_ROOT; ?>Cinema/ShowRemoveView">Listar Cines</a></li>
-      <li class="active"><a href="<?php echo FRONT_ROOT; ?>Movie/UpdateMovies">Actualizar Peliculas</a></li>
-      <li class="active"><a href="<?php echo FRONT_ROOT; ?>User/ShowRegisterAdminView">Registrar Administrador</a></li>
-      <li class="active"><a href="<?php echo FRONT_ROOT; ?>User/logout">Logout</a></li>        
-      <li class="active"><a href="<?php echo FRONT_ROOT; ?>Movie/ShowActiveMovies">Vista Usuario</a></li>
+    <nav class="list">
+      <li ><a href="<?php echo FRONT_ROOT; ?>Purchase/ShowSalesView">Totales vendidos</a></li>
+      <li ><a href="<?php echo FRONT_ROOT; ?>Purchase/ShowTicketsSoldView">Tickets vendidos</a></li>
+      <li ><a href="<?php echo FRONT_ROOT; ?>Movie/showMovies">Listar Peliculas</a></li>
+      <li ><a href="<?php echo FRONT_ROOT; ?>Cinema/ShowRemoveView">Listar Cines</a></li>
+      <li ><a href="<?php echo FRONT_ROOT; ?>Movie/UpdateMovies">Actualizar Peliculas</a></li>
+      <li ><a href="<?php echo FRONT_ROOT; ?>User/ShowRegisterAdminView">Registrar Administrador</a></li>
+      <li ><a href="<?php echo FRONT_ROOT; ?>User/logout">Logout</a></li>        
+      <li ><a href="<?php echo FRONT_ROOT; ?>Movie/ShowActiveMovies">Vista Usuario</a></li>
     </nav> 
   </header>
 </div>
-<div  class="hoc">
+<div >
