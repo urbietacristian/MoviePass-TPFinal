c98753294c3e639dfc85c554c08816b1f905e241
Cristian Urbieta <urbietacristian@gmail.com>
2022-02-07 17:50:13 -0300

    inputs
    correcciones menores en inputs

--- Views/admin/add_movieshow_1.php
+++ Views/admin/add_movieshow_1.php
@@ -1,25 +1,27 @@
 <?php
-require_once(VIEWS_PATH."navAdmin.php");
+require_once(VIEWS_PATH . "navAdmin.php");
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agregar Función - Paso 1</title>
 </head>
+
 <body>
-    <div align = 'center'>
-        <h1>Agregar función para "<?php echo $movie->getName()?>"</h1>
-        <form action="<?php echo FRONT_ROOT ?>MovieShow/ShowAddFunctionCinema2/<?php echo $id_movie ?>" method='post' >
+    <div align='center'>
+        <h1>Agregar función para "<?php echo $movie->getName() ?>"</h1>
+        <form action="<?php echo FRONT_ROOT ?>MovieShow/ShowAddFunctionCinema2/<?php echo $id_movie ?>" method='post'>
             <h1>Cine:</h1>
             <br>
             <select type="select" name="id_cinema" required>
                 <?php
-                foreach($cinemaList as $cinema){
+                foreach ($cinemaList as $cinema) {
                 ?>
-                <option value = "<?php echo $cinema->getId() ?>"><?php echo $cinema->getName()?></option>
+                    <option value="<?php echo $cinema->getId() ?>"><?php echo $cinema->getName() ?></option>
                 <?php
                 }
                 ?>
@@ -27,13 +29,14 @@ require_once(VIEWS_PATH."navAdmin.php");
             <br><br>
             <h1>Fecha</h1>
             <br>
-            <input type="date" name="date" value = <?php echo date('Y-m-d'); ?>  min = <?php echo date('Y-m-d'); ?> required/>
-            <input type="hidden" name="id_movie" value='<?php echo $id_movie?>' />
-            <br><br><br>            
+            <input type="date" name="date" value=<?php echo date('Y-m-d'); ?> min=<?php echo date('Y-m-d', strtotime('+1days')); ?> required />
+            <input type="hidden" name="id_movie" value='<?php echo $id_movie ?>' />
+            <br><br><br>
 
             <button type="submit">Aceptar</button>
         </form>
     </div>
-    
+
 </body>
+
 </html>
--- Views/admin/add_movieshow_2.php
+++ Views/admin/add_movieshow_2.php
@@ -1,25 +1,27 @@
 <?php
-require_once(VIEWS_PATH."navAdmin.php");
+require_once(VIEWS_PATH . "navAdmin.php");
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agregar Función - Paso 2</title>
 </head>
+
 <body>
-    <div align = 'center'>
-        <h1>Agregar Función para "<?php echo $movie->getName();?>" en <?php echo $cinema->getName();?> </h1>
-        <form action="<?php echo FRONT_ROOT ?>MovieShow/ShowAddFunctionCinemaEnd/<?php echo $id_movie; ?>" method='post' >
+    <div align='center'>
+        <h1>Agregar Función para "<?php echo $movie->getName(); ?>" en <?php echo $cinema->getName(); ?> </h1>
+        <form action="<?php echo FRONT_ROOT ?>MovieShow/ShowAddFunctionCinemaEnd/<?php echo $id_movie; ?>" method='post'>
             <h1>Sala:</h1>
             <br>
             <select type="select" name="id_room" required>
                 <?php
-                foreach($roomList as $room){
+                foreach ($roomList as $room) {
                 ?>
-                <option value = "<?php echo $room->getId();?>"><?php echo $room->getName();?></option>
+                    <option value="<?php echo $room->getId(); ?>"><?php echo $room->getName(); ?></option>
                 <?php
                 }
                 ?>
@@ -27,15 +29,16 @@ require_once(VIEWS_PATH."navAdmin.php");
             <br><br>
             <h1>Horario</h1>
             <br>
-            <input type="time" name="time" min="09:00:00" max="23:00:00" required>
+            <input type="time" name="time" value="12:00:00" min="09:00:00" max="23:00:00" required>
             <br><br><br>
-            <input type="hidden" name='id_cinema' value="<?php echo $id_cinema?>">            
-            <input type="hidden" name='date' value="<?php echo $date?>">
-            <input type="hidden" name='id_movie' value="<?php echo $id_movie?>">
+            <input type="hidden" name='id_cinema' value="<?php echo $id_cinema ?>">
+            <input type="hidden" name='date' value="<?php echo $date ?>">
+            <input type="hidden" name='id_movie' value="<?php echo $id_movie ?>">
 
             <button type="submit">Aceptar</button>
         </form>
     </div>
-    
+
 </body>
+
 </html>
