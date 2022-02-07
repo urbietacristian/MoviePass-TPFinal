8e2e552a695f0bca8d31fdde0509372ba0542683
Cristian Urbieta <urbietacristian@gmail.com>
2022-02-07 04:37:41 -0300

    Nuevas visuales en vistas

--- Views/user/list_movies_by_cinema.php
+++ Views/user/list_movies_by_cinema.php
@@ -1,26 +1,25 @@
 <?php
 require_once(VIEWS_PATH."navUser.php");
 ?>
-<main class="d-flex align-items-center justify-content-center height-100" >
-    <div id="mainav">
+
+<div class="movies">
     <h1>Películas en <?php echo $cinema->getName()?></h1>
+    <ul class="carta-grid" >
         <?php        
         foreach($movie_list as $movie){
-        ?>                        
-        <li>            
-            <div class='card'>
-                <a href='<?php echo FRONT_ROOT; ?>MovieShow/ShowFunctionsByMovie?id_movie=<?php echo $movie->getIdApi()?>&cinema_name=<?php echo $cinema->getName()?>'>
-                <div >
-                    <img src='http://image.tmdb.org/t/p/w300<?php echo $movie->getImage()?>'>
-                </div>
-                <div class='title' style='display:block;text-overflow: ellipsis;width: 270px;overflow: hidden; white-space: nowrap'>
-                    <p><?php echo $movie->getName()?></p>
-                </div>
-                </a>
+        ?>         
+        <div class='carta'>
+            <a href='<?php echo FRONT_ROOT; ?>MovieShow/ShowFunctionsByMovie?id_movie=<?php echo $movie->getIdApi()?>&cinema_name=<?php echo $cinema->getName()?>'>
+            <div class="carta-body carta-image">
+                <img  src='http://image.tmdb.org/t/p/w300<?php echo $movie->getImage()?>'>
             </div>
-        </li>
+            <div class="carta-footer">
+                <p><?php echo $movie->getName()?></p>
+            </div>
+            </a>
+        </div>
         <?php
         }
         ?>
-    </div>
-</main>
+    </ul>
+</div>
